9d254d70e21e1a0ed50b76dc90cb9e535a635431
Thomas Dao <thomas.daoxuandung@gmail.com>
2012-04-25 22:07:17 +0800

    Add view manager: create new view while auto clean previous view, reuse existing view

---
+++ vm.js
@@ -0,0 +1,38 @@
+// Define View Manager to manage view states
+window.VM = window.VM || {};
+
+// VM.views hold all references to existing views
+VM.views = VM.views || {};
+
+// VM.createView always cleans up existing view before
+// creating a new one.
+// callback function always return a new view instance
+VM.createView = function(name, callback) {
+	if (typeof VM.views[name] !== 'undefined') {
+		// Cleanup view
+		// Remove all of the view's delegated events
+		VM.views[name].undelegateEvents();
+		// Remove view from the DOM
+		VM.views[name].remove();
+		// Removes all callbacks on view
+		VM.views[name].off();
+
+		if (typeof VM.views[name].close === 'function') {
+			VM.views[name].close();
+		}
+	}
+	VM.views[name] = callback();
+	return VM.views[name];
+}
+
+// VM.reuseView always returns existing view. Otherwise it
+// execute callback function to return new view
+// callback function always return a new view instance
+VM.reuseView = function(name, callback) {
+	if (typeof VM.views[name] !== 'undefined') {
+		return VM.views[name];
+	}
+	
+	VM.views[name] = callback();
+	return VM.views[name];
+}
